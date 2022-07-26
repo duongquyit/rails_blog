97e5639798fcbe9d9ec66831a319b03484f23ba4
Duong Quy <xxboy.tlxx@gmail.com>
2022-07-26 16:47:46 +0700

    change default mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,29 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
  
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  layout :layout_by_resource
  
   protected
  
-  def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
 
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  end
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+    
+    def layout_by_resource
+      if devise_controller?
+        if resource_name == :admin
+          "devise_admin_application"
+        elsif resource_name == :user
+          "devise_user_application"
+        else
+          "devise_application"
+        end
+        "application"
+      end
+    end
+  
 end
---
+++ app/mailers/my_mailer.rb
@@ -0,0 +1,4 @@
+class MyMailer < Devise::Mailer
+  default from: 'xxxx@example.com'
+  default reply_to: 'to---xxxx@example.com'
+end
